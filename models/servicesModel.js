8bac15c14e5bac372a7a66fe6e1486f0ee686b11
mustaphachqoubi <mustaphachqoubi88@gmail.com>
2023-06-14 16:07:57 +0100

    i edited the types of the services model

--- models/servicesModel.js
+++ models/servicesModel.js
@@ -4,8 +4,8 @@ const Schema = mongoose.Schema
 
 const optionsSchema = new Schema({
   name: { type: String, require: true },
-  price: { type: String, require: true },
-  number: { type: String, require: true}
+  price: { type: Number, require: true },
+  number: { type: Number, require: true}
 })
 
 const featuresSchema = new Schema({
@@ -15,15 +15,15 @@ const featuresSchema = new Schema({
 const itemsSchema = new Schema({
   title: { type: String, require: true },
   features: { type: [ featuresSchema ], require: true },
-  price: { type: String, require: true },
+  price: { type: Number, require: true },
   options: { type: [ optionsSchema ], require: false },
-  number: { type: String, require: true}
+  number: { type: Number, require: true}
 }) 
 
 const servicesSchema = new Schema({
   title: {type: String, require: true},
   items: {type: [ itemsSchema ], require: true},
-  number: { type: String, require: true}
+  number: { type: Number, require: true}
 }, { timestamps: true })
 
 module.exports = mongoose.model('Services', servicesSchema)
